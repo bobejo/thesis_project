b068b7a5bc3da8ea4c247ac36fb529f623bca291
bobejo <samuel.ingemarsson@gmail.com>
2018-02-01 09:47:54 +0100

    Master

--- computer_vision/crop_images.py
+++ computer_vision/crop_images.py
@@ -6,11 +6,9 @@ import cv2
 
 def crop_images(path):
     image_list = glob.glob(path)
-    # image_list2=os.listdir(path)
     print(image_list)
     for image in image_list:
         img = cv2.imread(image, 1)
-        # print(img)
         cropped_image = img[100:200, 20:150]
         print('Size of the  image:' + str(img.shape))
         print('Size of the cropped image:' + str(cropped_image.shape))
@@ -19,5 +17,4 @@ def crop_images(path):
         cv2.imwrite(image, cropped_image)
 
 
-# crop_images("/home/saming/Documents/Master/images/*.jpg")
 crop_images("/home/saming/Documents/Master/images/*.jpg")
--- computer_vision/snap_pic.py
+++ computer_vision/snap_pic.py
@@ -23,4 +23,3 @@ def get_time():
 
 
 main()
-"imges/left14:15:00.jpg"
