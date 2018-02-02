a5d7c9a518b6bdfb698eea32bccca680d27740e5
bobejo <samuel.ingemarsson@gmail.com>
2018-02-02 14:52:56 +0100

    a,b=path.split does not work for python versions > 2.7

--- computer_vision/crop_images.py
+++ computer_vision/crop_images.py
@@ -8,20 +8,20 @@ def crop_images(path):
 
     for image_path in image_path_list:
         img = cv2.imread(image_path, 1)
-        a, b = image_path.split("images/")
+        a = image_path.split("images/")
 
         if image_path.find('left') >= 0:
             cropped_image = img[500:900, 600:900]  # Choose these y,x
-            image_path = a + "images/" + "cropped_images/" + b
+            image_path = a[0] + "images/" + "cropped_images/" + a[1]
             cv2.imwrite(image_path, cropped_image)
 
         elif image_path.find('right') >= 0:
             cropped_image = img[500:900, 1050:1350]
-            image_path = a + "images/" + "cropped_images/" + b
+            image_path = a[0] + "images/" + "cropped_images/" + a[1]
             cv2.imwrite(image_path, cropped_image)
 
         else:
-            print("File: " + b + " skipped")
+            print("File: " + a[1] + " skipped")
 
 
 
