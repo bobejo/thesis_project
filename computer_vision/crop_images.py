19f5bab91f4e82736b18247703239fc4ed3694c6
bobejo <samuel.ingemarsson@gmail.com>
2018-02-08 13:44:14 +0100

    Master

--- computer_vision/crop_images.py
+++ computer_vision/crop_images.py
@@ -4,6 +4,13 @@ import cv2
 
 
 def crop_images(path):
+    """
+    Takes multiple images and crops to each blue box.
+    Cropping sizes are chosen manually
+
+    :param path: The path to the folder where the images are
+    :return:
+    """
     image_path_list = glob.glob(path)
 
     for image_path in image_path_list:
@@ -11,19 +18,35 @@ def crop_images(path):
         a = image_path.split("images/")
 
         if image_path.find('left') >= 0:
-            cropped_image = img[500:900, 600:900]  # Choose these y,x
-            image_path = a[0] + "images/" + "cropped_images/" + a[1]
+            cropped_image = img[400:900, 260:610]  # Choose these y,x
+            image_path = a[0] + "images/" + "cropped_images/oilpipe" + a[1]
             cv2.imwrite(image_path, cropped_image)
 
+            cropped_image = img[400:900, 585:935]  # Choose these y,x
+            image_path = a[0] + "images/" + "cropped_images/oilfilter" + a[1]
+            cv2.imwrite(image_path, cropped_image)
+
+            cropped_image = img[400:900, 950:1300]  # Choose these y,x
+            image_path = a[0] + "images/" + "cropped_images/pipe" + a[1]
+            cv2.imwrite(image_path, cropped_image)
+
+
         elif image_path.find('right') >= 0:
-            cropped_image = img[500:900, 1050:1350]
-            image_path = a[0] + "images/" + "cropped_images/" + a[1]
+            cropped_image = img[400:900, 725:1075]  # Choose these rows,column
+            image_path = a[0] + "images/" + "cropped_images/oilpipe" + a[1]
+            cv2.imwrite(image_path, cropped_image)
+
+            cropped_image = img[425:925, 1050:1400]  # Choose these y,x
+            image_path = a[0] + "images/" + "cropped_images/oilfilter" + a[1]
+            cv2.imwrite(image_path, cropped_image)
+
+            cropped_image = img[400:900, 1400:1750]  # Choose these y,x
+            image_path = a[0] + "images/" + "cropped_images/pipe" + a[1]
             cv2.imwrite(image_path, cropped_image)
 
         else:
             print("File: " + a[1] + " skipped")
 
 
-
-#Choose where the camera files are
+# Choose where the camera files are
 crop_images('/home/saming/PycharmProjects/thesis_project/computer_vision/images/*.jpg')
