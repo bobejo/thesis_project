fe75e4d084bc8919f1070fc135d5446a206ca9cc
bobejo <samuel.ingemarsson@gmail.com>
2018-03-23 16:42:12 +0100

    Added matrices

--- computer_vision/paths.py
+++ computer_vision/paths.py
@@ -12,18 +12,16 @@ x_test_path = 'C:\\Users\\Samuel\\GoogleDrive\Master\Python\\thesis_project\\com
 y_test_path = 'C:\\Users\\Samuel\\GoogleDrive\Master\Python\\thesis_project\\computer_vision\\images\\Training_data\\test\\target'
 
 # The verification images. Make sure that these are not used for training.
-x_verification_path='C:\\Users\\Samuel\\GoogleDrive\Master\Python\\thesis_project\\computer_vision\\images\\Verification_data\\x_test\\test'
-y_verification_path='C:\\Users\\Samuel\\GoogleDrive\Master\Python\\thesis_project\\computer_vision\\images\\Training_data\\y_test\\test'
+x_verification_path = 'C:\\Users\\Samuel\\GoogleDrive\Master\Python\\thesis_project\\computer_vision\\images\\Verification_data\\x_test\\test'
+y_verification_path = 'C:\\Users\\Samuel\\GoogleDrive\Master\Python\\thesis_project\\computer_vision\\images\\Training_data\\y_test\\test'
 
 # The path to the model
 model_path = 'C:\\Users\\Samuel\\GoogleDrive\\Master\\Python\\thesis_project\\computer_vision\\models\\simple_model.h5'
-
 
 ## Triangulation
 # The path to the camera matrices
 left_matrix_path = 'CameraMatrix\\leftMatrix.npy'
 right_matrix_path = 'CameraMatrix\\rightMatrix.npy'
-
 
 ## Testing
 # Used for affine transformation testing
@@ -35,3 +33,18 @@ test_path_right1 = 'C:\\Users\\Samuel\\Desktop\\pipes\\left\\images\\croppedleft
 test_path_left1 = 'C:\\Users\\Samuel\\Desktop\\pipes\\right\\images\\croppedright_pipe08_18_23.jpg'
 test_path_right2 = 'C:\\Users\\Samuel\\Desktop\\pipes\\left\\images\\left_pipe08_18_23.jpg'
 test_path_left2 = 'C:\\Users\\Samuel\\Desktop\\pipes\\right\\images\\right_pipe08_18_23.jpg'
+
+# Snap_pic
+# Folder where the images will be placed
+save_path = 'C:\\Users\\Samuel\\GoogleDrive\\Master\\Python\\thesis_project\\\computer_vision\\images\\main'
+
+# Affine transformation
+# Path to left and right images with chessboard
+# Gives points that will be used in the ransac leastsquare solver
+left_chessboard = save_path + '\\left\\leftcalibration09_38_49.jpg'
+right_chessboard = save_path + '\\right\\rightcalibration09_38_49.jpg'
+
+# Cropping
+# VLH
+lcrop = [(200, 750), (180, 550)]
+rcrop = [(125, 675), (530, 900)]
