3ccaaf4c03d589b3781e0cea1a6821b2d8a944ae
bobejo <samuel.ingemarsson@gmail.com>
2018-03-23 10:18:06 +0100

    The main program

---
+++ computer_vision/main.py
@@ -0,0 +1,50 @@
+import cnn
+import image_manipulation as im
+import grasp_finder as gf
+import snap_pic as sp
+import paths
+import keras
+import glob
+import global_variables as gv
+import image_registration as ir
+import numpy as np
+
+
+def find_3D_point():
+    """
+    Finds the best grasping point and angle. Requires the relationship between left and right images
+    :return: The 3D gripping point in robot base frame and the gripping angle.
+    """
+    "Take the images and crop them"
+    sp.snap_pic('', paths.save_path)
+    im.crop_images(paths.save_path, gv.lcrop, gv.rcrop)
+    crop_path = paths.save_path + '*croppedleft'
+    cropped_left = glob.glob(crop_path)  # The left cropped image
+
+    "Load trained model and run prediction"
+    model = keras.models.load_model(paths.model_path)
+    p = cnn.get_prediction(model, cropped_left[0])
+
+    "Convert to binary image and dilate"
+    p = im.image_segmentation(p[0], gv.threshold, gv.dilation_size)
+
+    "Find the 2d gripping point and angle for the left image"
+    _, gripping_point_left, gripping_angle = gf.contour_detector(p)
+
+    "Find the corresponding point of the right camera"
+
+    gripping_point_right = ir.affine_transformation(gv.A, gv.t, gripping_point_left)
+
+    "Finds the 3D point in camera frame, using triangulation, and convert to base frame."
+
+    # Load camera matrices
+    lcm = np.genfromtxt(paths.left_matrix_path)
+    rcm = np.genfromtxt(paths.right_matrix_path)
+
+    # Triangulate
+    tri_camera = gf.triangulate_point(gripping_point_left, gripping_point_right, lcm, rcm)
+
+    # Convert to base frame
+    gripping_point_base = ir.camera_transform(gv.T, tri_camera)
+
+    return gripping_point_base, gripping_angle
