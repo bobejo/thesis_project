48b509821c9efaa1dfa70b2ff42afbb193c71115
bobejo <samuel.ingemarsson@gmail.com>
2018-03-29 10:11:15 +0200

    Added chessboard

--- computer_vision/camera_calibrator.m
+++ computer_vision/camera_calibrator.m
@@ -2,14 +2,19 @@
 % Information how to use the tool can be found here https://mathworks.com/help/vision/ug/stereo-camera-calibrator-app.html
 
 stereoCameraCalibrator
-%%  
+%% Create camera matrix
 %  Create the camera matrix with the intrinsic and extrinsic matrices.
 %  Saves the matrices as .txt
 clc
 R1=diag([1,1,1]);
-R2=stereoParams.RotationOfCamera2;
 t1=[0 0 0]';
-t2=stereoParams.TranslationOfCamera2'-[0;0;35];
+R2=stereoParams.RotationOfCamera2;
+t2=stereoParams.TranslationOfCamera2';
+
+% Adjust if wrong from calibration
+% t2(1)=530;
+%t2(2)=0;
+ %t2(3)=0;
 
 R2b=[R2;0 0 0];
 R1b=[R1;0 0 0];
@@ -26,17 +31,33 @@ P1=K1*extrinsic1;
 P2=K2*extrinsic2;
 
 
-save('lcm_vlh.txt','P1','-ascii');
-save('rcm_vlh.txt','P2','-ascii');
+save('lcm_vlh2.txt','P1','-ascii');
+save('rcm_vlh2.txt','P2','-ascii');
+%% Detect intersections between squares
+clc
+left=imread('images\Chessboard_images\leftcalibration07_16_21.jpg');
+right=imread('images\Chessboard_images\rightcalibration07_16_21.jpg');
+[left2,~] = undistortImage(left,stereoParams.CameraParameters1);
+[right2,~] = undistortImage(right,stereoParams.CameraParameters2);
+
+[imagePointsLeft,~] = detectCheckerboardPoints(left);
+[imagePointsRight,~] = detectCheckerboardPoints(right);
+
+% Undistort them
+imagePointsLeft2 = undistortPoints(imagePointsLeft,stereoParams.CameraParameters1);
+imagePointsRight2 = undistortPoints(imagePointsRight,stereoParams.CameraParameters2);
 %%
+save('lpoints3.txt','imagePointsLeft2','-ascii')
+save('rpoints3.txt','imagePointsRight2','-ascii')
 
-left=imread('leftcalibration09_38_49.jpg');
-right=imread('rightcalibration09_38_49.jpg');
-
-[imagePointsLeft,boardSizeLeft] = detectCheckerboardPoints(left);
-[imagePointsRight,boardSizeRight] = detectCheckerboardPoints(right);clc
-save('lpoints.txt','imagePointsLeft','-ascii')
-save('rpoints.txt','imagePointsRight','-ascii')
-
+%% Plot the points
+figure(1)
+imshow(left);
+hold on;
+plot(imagePointsLeft2(:,1),imagePointsLeft2(:,2),'bo');
+figure(2)
+imshow(right);
+hold on;
+plot(imagePointsRight2(:,1),imagePointsRight2(:,2),'bo');
 
 
