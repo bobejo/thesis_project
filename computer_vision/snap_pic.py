dabbac8fa919595134885e03636822f5059fd951
bobejo <samuel.ingemarsson@gmail.com>
2018-04-06 14:21:58 +0200

    Main

--- computer_vision/snap_pic.py
+++ computer_vision/snap_pic.py
@@ -1,16 +1,16 @@
 import os
 import time as time
 from time import gmtime
+import paths
 
-save_path = 'C:\\Users\\Samuel\\GoogleDrive\\Master\\Python\\thesis_project\\camera_calibration'
 
-
-def snap_pic(name):
+def snap_pic(name, save_path):
     """
     Takes two images one for each camera and saves them to the images/ folder.
     Requires program Wget.
 
     :param name: Save name of the file
+    :param save_path: The folder where the images should be placed.
     """
     t = get_time()
     os.system(
@@ -18,6 +18,7 @@ def snap_pic(name):
     print("Picture 1 left is saved")
     os.system('wget http://admin:@192.168.1.144/dms?nowprofileid=1 -O' '' + save_path + "\\right" + name + t + ".jpg")
     print("Picture 1 right is saved")
+    return save_path + "\\left" + name + t + ".jpg", save_path + "\\right" + name + t + ".jpg"
 
 
 def get_time():
@@ -31,4 +32,4 @@ def get_time():
     return t[13:21]
 
 # Choose name of pipe
-# snap_pic("Calibration")
+#snap_pic("calibration", paths.save_path)
