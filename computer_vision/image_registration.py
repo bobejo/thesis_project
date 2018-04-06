45cae64d02dfba02779b7ecdfb49da450a6d0294
bobejo <samuel.ingemarsson@gmail.com>
2018-04-06 12:54:01 +0200

    base_transform

--- computer_vision/image_registration.py
+++ computer_vision/image_registration.py
@@ -5,18 +5,18 @@ import cv2
 import paths
 
 
-def camera_transform(T, U_camera):
+def base_transform(T, U_camera):
     """
     Takes camera points and transform them to the UR10 base frame.
 
     :param T: Transformation matrix (3x4)
     :param U: Triangulated points in camera frame (X;Y;Z)
-    :return:
+    :return: The points in base frame
     """
 
     U_camera = np.vstack([U_camera, 1])
     T = np.vstack([T, [0, 0, 0, 1]])
-    return np.dot(T, U_camera)
+    return np.dot(T, U_camera)[:3]
 
 
 def affine_transformation(A, t, lpoints):
@@ -29,15 +29,56 @@ def affine_transformation(A, t, lpoints):
     :param lpoints: Points from left image
     :return: The corresponding points in the right image
     """
+
     mul = np.matmul(A.reshape(2, 2), lpoints)
-    rpoints = np.add(mul.reshape(2, 1), t)
-    return tuple((rpoints[0][0], rpoints[1][0]))
+
+    #rpoints = np.add(mul.reshape(2, 1), t)
+    rpoints = np.add(mul, t)
+    return tuple((rpoints[0], rpoints[1]))
+
+
+def projective_transformation(H, lpoints):
+    lpoints = np.vstack([lpoints.reshape(2, 1), 1])
+    mul = np.matmul(H, lpoints)
+
+    return tuple(mul[0], mul[1])
+
+
+def projective_transformation_solver(lpoints, rpoints):
+    """
+    Calculates the projection transformation matrix with 4 coordinates.
+
+    :param lpoints:  List of coordinates (x,y), of at least length 4, for left camera
+    :param rpoints: List of coordinates (x,y), of at least length 4, for right camera
+    :return: Transformation matrix H
+    """
+
+    M = np.matrix(
+        [[lpoints[0][0], lpoints[0][1], 1, 0, 0, 0, -rpoints[0][0] * lpoints[0][0], -rpoints[0][0] * lpoints[0][1]],
+         [0, 0, 0, lpoints[0][0], lpoints[0][1], 1, -rpoints[0][1] * lpoints[0][0], -rpoints[0][1] * lpoints[0][1]],
+         [lpoints[1][0], lpoints[1][1], 1, 0, 0, 0, -rpoints[0][1] * lpoints[0][0], -rpoints[0][1] * lpoints[0][1]],
+         [0, 0, 0, lpoints[1][0], lpoints[1][1], 1, -rpoints[0][1] * lpoints[0][0], -rpoints[0][1] * lpoints[0][1]],
+         [lpoints[2][0], lpoints[2][1], 1, 0, 0, 0, -rpoints[0][1] * lpoints[0][0], -rpoints[0][1] * lpoints[0][1]],
+         [0, 0, 0, lpoints[2][0], lpoints[2][1], 1, -rpoints[0][1] * lpoints[0][0], -rpoints[0][1] * lpoints[0][1]]]
+    )
+
+    v = np.matrix(
+        [[rpoints[0][0]], [rpoints[0][1]], [rpoints[1][0]], [rpoints[1][1]], [rpoints[2][0]],
+         [rpoints[2][1]]])
+    try:
+        theta = solve(M, v)
+    except np.linalg.linalg.LinAlgError:
+        return np.array([0, 0, 0, 0]), np.array([0, 0])
+
+    A = np.array([theta[0], theta[1], theta[2], theta[3]])
+    A = A.reshape(2, 2)
+    t = np.array([theta[4], theta[5]])
+    return A, t
 
 
 def affine_transformation_solver(lpoints, rpoints):
     """
     Calculates the transformation matrix and translation vector with 3 coordinates.
-    theta=M\v
 
     :param lpoints:  List of coordinates (x,y), of at least length 3, for left camera
     :param rpoints: List of coordinates (x,y), of at least length 3, for right camera
@@ -68,7 +109,7 @@ def affine_transformation_solver(lpoints, rpoints):
 
 def get_residuals(A, t, lpoints, rpoints):
     """
-    Estimates the residuals for the transformation
+    Estimates the residuals for the transformation. Used in ransac_inliers for finding good combinations of points.
 
     :param A: Transformation matrix
     :param t: Translation vector
@@ -98,8 +139,7 @@ def ransac_inliers(lpoints, rpoints, threshold):
     N = len(lpoints)
     new_lpoints = []
     new_rpoints = []
-    loop_length = 100 + 50 * N
-    all_loss = []
+    loop_length = 100 + 100 * N
 
     for i in range(0, loop_length):
         lsample = []
@@ -107,7 +147,7 @@ def ransac_inliers(lpoints, rpoints, threshold):
         index = []
         j = 0
         while j < 3:
-            rand_index = random.randrange(0, len(lpoints) - 3)
+            rand_index = random.randrange(0, len(lpoints) - 1)
             if rand_index not in index:
                 lsample.append(lpoints[rand_index])
                 rsample.append(rpoints[rand_index])
@@ -116,7 +156,6 @@ def ransac_inliers(lpoints, rpoints, threshold):
         [A, t] = affine_transformation_solver(lsample, rsample)
 
         loss = get_residuals(A, t, lpoints, rpoints)
-        all_loss = all_loss + [loss]
 
         if loss < threshold:
             new_lpoints.append(lsample)
@@ -141,7 +180,8 @@ def ransac_inliers(lpoints, rpoints, threshold):
 def least_square_solver(lpoints, rpoints, threshold):
     """
     Uses the least square method the estimate the transformation matrix and translation vector.
-    First removes outliers using ransac_inliers
+    Least square is sensitive to outliers therefor ransac_inliers is first used to remove them.
+
 
     :param lpoints: List of coordinates (x,y), of at least length 3, for left camera
     :param rpoints: List of coordinates (x,y), of at least length 3, for right camera
@@ -174,9 +214,11 @@ def find_image_transformation(chess=True):
     """
     Takes two chessboard images, left and right, and finds the corner of the chessboard. These are then used for
     the least_square_solver to find the transformation matrix
+    Adjust the ransac threshold!
     :param chess: If the function shall create chessboard corner points
     :return: transformation matrix and translation vector
     """
+    ransac__threshold = 130
     if chess:
         left_chess = cv2.imread(paths.left_chessboard)
         right_chess = cv2.imread(paths.right_chessboard)
@@ -188,12 +230,18 @@ def find_image_transformation(chess=True):
         if len(corners_left) == len(corners_right):
             corners_left = [cl[0] for cl in corners_left]
             corners_right = [cr[0] for cr in corners_right]
-            A, t = least_square_solver(corners_left, corners_right, 100)
+            A, t = least_square_solver(corners_left, corners_right, ransac__threshold)
             return A, t
         else:
+
             print('Different amount of chessboard points')
     else:
-        corners_left = np.genfromtxt('lpoints.txt')
-        corners_right = np.genfromtxt('rpoints.txt')
-        A, t = least_square_solver(corners_left, corners_right, 100)
+        corners_left = np.genfromtxt('lpoints5.txt')
+        corners_right = np.genfromtxt('rpoints5.txt')
+        print(len(corners_left))
+        print(len(corners_right))
+        A, t = least_square_solver(corners_left, corners_right, ransac__threshold)
         return A, t
+
+
+#print(find_image_transformation(chess=False))
