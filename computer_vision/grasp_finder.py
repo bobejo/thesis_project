cc498de7f7597a8d8acbd340688fba67c6183990
bobejo <samuel.ingemarsson@gmail.com>
2018-04-16 15:14:40 +0200

    Added circle detection

--- computer_vision/grasp_finder.py
+++ computer_vision/grasp_finder.py
@@ -7,12 +7,46 @@ import numpy as np;
 from matplotlib import pyplot as plt
 from scipy import ndimage
 import operator
+import image_manipulation as im
+
+def circle_detector(img):
+    output = img.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.8, 100, 150, 100, 65, 50, 65)
+    if circles is not None:
+        # convert the (x, y) coordinates and radius of the circles to integers
+        circles = np.round(circles[0, :]).astype("int")
+
+        # loop over the (x, y) coordinates and radius of the circles
+
+        for (x, y, r) in circles:
+            # draw the circle in the output image, then draw a rectangle
+            # corresponding to the center of the circles
+            cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+            cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+        dist = 0
+        best_i=0
+        for i in range(0, len(circles)):
+            temp_dist = 0
+            for j in range(0, len(circles)):
+                temp_dist += np.sqrt(
+                    (np.square(circles[i][0] - circles[j][0]) + np.square(circles[i][1] - circles[j][1])))
+                if temp_dist > dist:
+                    dist = temp_dist
+                    best_i = i
+
+        cv2.rectangle(output, (circles[best_i][0] - 5, circles[best_i][1] - 5),
+                      (circles[best_i][0] + 5, circles[best_i][1] + 5), (255, 0, 255), -1)
+
+    return (int(circles[best_i][0]), int(circles[best_i][1])), output
 
 
 def contour_detector(img):
     """
-    Finds the contours of the image. Returns the binary image with the contour that have the largest area
+    Finds the contours of the image. Returns the binary image with the contour that have the longest length
      and the centroid of this area.
+     Center points are given from the image moments of the contour.
     :param img: A binary dilated numpy image
     :return: The input image with the contour, the centroid of this contour and the angle of the contour.
     """
@@ -23,14 +57,14 @@ def contour_detector(img):
     # Take the largest contour
     if not contours:
         print('Error No shapes found!')
-        return img
+        return img, (None, None), None, None
     for c in contours:
         length = cv2.arcLength(c, True)
         if length > length_contour:
             length_contour = length
             cont = c
 
-    cv2.drawContours(img, cont, -1, 128, 10)
+    cv2.drawContours(img, cont, -1, 128, 2)
 
     # The coordinates of the center of the contour from the moments.
     M = cv2.moments(cont)
@@ -38,84 +72,18 @@ def contour_detector(img):
     cy = int(M['m01'] / M['m00'])
     # Try to fit an ellipse inside the contour and give the angle of the largest one
     _, _, angle = cv2.fitEllipse(cont)
+    print(angle)
 
+    cv2.circle(img, (cx, cy),3,80,3)
     return img, (cx, cy), angle, cont
-
-
-def blob_detector(img):
-    """
-    Creates a blob detector and uses it to find the blobs of the binary image
-
-    :param img: Binary dilated image
-    :return: The keypoints for the blobs
-    """
-    #
-    # Settings for blobdetector
-    params = cv2.SimpleBlobDetector_Params()
-    params.filterByCircularity = True
-    params.minCircularity = 0.5
-    params.maxCircularity = 0.8
-    params.filterByColor = False
-    params.filterByConvexity = False
-    params.filterByInertia = False
-    params.minThreshold = 0
-    params.maxThreshold = 255
-    params.thresholdStep = 10
-    params.filterByArea = True
-    params.minArea = 20
-    # params.maxArea = 5000
-    params.minDistBetweenBlobs = 0
-
-    detector = cv2.SimpleBlobDetector_create(params)
-    keypoints = detector.detect(img)
-    keypoints.sort(key=operator.attrgetter('size'))
-
-    k = keypoints
-
-    return k
-
-
-def featurematching_coordinates(limg, rimg, threshold=10):
-    """
-    Extracts features from left and right image and matches them with each other to find similarities.
-    Sorts the matches and return the coordinates off all matches.
-
-    :param limg:   The image from the left camera
-    :param rimg:   The image from the right camera
-    :param threshold: The maximum distance between descriptors
-    :return: (lpoints, rpoints): The coordinates for each match in left image and right image
-    """
-
-    if type(limg) == str:
-        limg = cv2.imread(limg, 0)
-        rimg = cv2.imread(rimg, 0)
-
-    orb = cv2.ORB_create()
-    kp1, des1 = orb.detectAndCompute(limg, None)
-    kp2, des2 = orb.detectAndCompute(rimg, None)
-
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(des1, des2)
-    good = []
-
-    # Only take those with a good match
-    for m in matches:
-        if m.distance < threshold:
-            good.append([m])
-
-    good = sorted(good, key=lambda x: x[0].distance)
-
-    img3 = cv2.drawMatchesKnn(limg, kp1, rimg, kp2, good, None, flags=2)
-    plt.imshow(img3), plt.show()
-    lpoints = [kp1[mat[0].queryIdx].pt for mat in good]
-    rpoints = [kp2[mat[0].trainIdx].pt for mat in good]
-
-    return lpoints, rpoints
 
 
 def triangulate_point(lpoint, rpoint, left_cm, right_cm):
     """
     Uses triangulation to generate the global 3D coordinates using two 2D (x,y) pixel coordinates.
+    Finds the unknown X, Y, Z and lamda by solving two pinhole equations: lamda*(x;y;1)=P*(X;Y;Z;1).
+
+
 
     :param lpoint: The pixels of the left image
     :param rpoint: The pixels of the right image
@@ -123,7 +91,8 @@ def triangulate_point(lpoint, rpoint, left_cm, right_cm):
     :param right_cm: The camera matrix for the right camera
     :return: 3D point in left camera frame
     """
-
+    print(lpoint)
+    print(rpoint)
     theta = cv2.triangulatePoints(left_cm, right_cm, lpoint, rpoint)
 
     if theta[3] == 0:
